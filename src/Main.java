c9c0da891f5c6589d445d6ab5980abe7165f390f
fdia74 <fdia@mail.ru>
2021-10-26 19:47:39 +0300

    java-project1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello programming!");
+        int price = 2_000_000_000;
+        int count = 500_000_000;
+        int total = price * count;
+        System.out.println(total);
     }
 }
